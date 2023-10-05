b1022334105c30d875201afcabb3867753f5fa22
garima verma <114860130+garimaver@users.noreply.github.com>
2023-10-05 14:23:01 +0530

    added styling

--- script.js
+++ script.js
@@ -69,27 +69,20 @@ function validateName(){
 function validatePhone(){
     var phone = document.getElementById('contact-phone').value;
 
-
     if(phone.length==0){
         phoneError.innerHTML= 'Phone is required';
         return false;
     }
-
-
-    
+  
     if(phone.length !== 0){
         phoneError.innerHTML= 'Phone no should be 10 digits';
         return false;
     }
 
-
-
     if(!phone.match(/^[0-9]{10}$/)){
        phoneError.innerHTML = 'only digits please';
        return false;
     }
-
-
     phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
     return true;
 }   
@@ -99,20 +92,42 @@ function validateEmail(){
     var email = document.getElementById('contact-email').value;
 
 
-    if(email.length==0){
+    if(email.length == 0){
         emailError.innerHTML='Email is required';
         return false;
     }
 
 
-    if(email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML='Email is Invalid'
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML='Email is Invalid';
     }
 
 
     emailError.innerHTML='<i class="fa-solid fa-circle-check"></i>';
     return true;    
 }
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
 
+    if (left > 0) {
+        messageError.innerHTML = left + 'more characters required';
+        return false;
+    }
+
+  messageError.innerHTML='<i class="fa-solid fa-circle-check"></i>';
+    return true;
+
+}
+
+function validateForm () {
+    if(!validateName() || !validatePhone()|| !validateEmail() || !validateMessage()){
+        submitError.style.display ='block';
+        submitError.innerHTML = 'please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';},3000);
+        return false;
+    }
+}
 
 
